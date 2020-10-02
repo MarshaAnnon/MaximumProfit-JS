28fee179ee14415eb2ba34f85c88fbbb34526140
marsha <marshaannon@gmail.com>
2020-10-02 15:41:51 -0400

    expected 16, getting 11

--- MaximumProfit.js
+++ MaximumProfit.js
@@ -1,6 +1,33 @@
 class MaximumProfit {
+    // input = [45, 24, 35, 31, 40, 38, 11];
+
     find(input){
-        return null;
+        let buyPrice = 0;
+        let sellPrice = 0;
+        let maxProfit = -1; //default for maxProfit
+        for(let i = 0; i < input.length -1; i++) {
+            
+
+            if (input[i] < input[i+1]) {
+                buyPrice = input[i]
+                sellPrice = input[i+1]
+                
+                let profit = (sellPrice - buyPrice);
+                
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+
+            if (buyPrice > input[i+1]) {
+                buyPrice = input[i+1];
+            } else {
+                sellPrice = input[i+1];
+            }
+            
+        }
+        // console.log(maxProfit)//maxProfit is 24
+        return maxProfit;
     }
 }
 
